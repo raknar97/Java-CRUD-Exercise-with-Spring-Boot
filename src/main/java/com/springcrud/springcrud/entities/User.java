a8228bb7eb402b7c7bd7e955db391eb5bf9f9dbe
Alex <alex.yova97@gmail.com>
2020-03-04 15:13:24 +0100

    Creates User entity

---
+++ src/main/java/com/springcrud/springcrud/entities/User.java
@@ -0,0 +1,30 @@
+package com.springcrud.springcrud.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private Date birthdate;
+
+    public User(Integer id, String name, Date birthdate) {
+        this.id = id;
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+}
+
